ba80f63905983449a23208921ed5ad1adf391c33
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-03-28 22:20:08 +0300

    change on configureStore()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+// import { rootReducer } from './reducer';
+import { tasksReducer, filtersReducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filter: filtersReducer,
+  },
+});
